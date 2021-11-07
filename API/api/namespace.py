75eecf73c066a7bc4f5e0afbf5e2e6ab2e9001f5
kimbk209 <64012235+kimbk209@users.noreply.github.com>
2021-11-08 02:25:17 +0900

    인덴테이션 문제 해결

--- API/api/namespace.py
+++ API/api/namespace.py
@@ -3,54 +3,64 @@ from kubernetes.client.rest import ApiException
 from api import core_v1
 #import time
 
-namespace_api = Blueprint('namespace',__name__)
+namespace_api = Blueprint('namespace', __name__)
+
 
 @namespace_api.route('/<namespace>', methods=['POST', 'GET', 'DELETE'])
 def namespace(namespace):
-	if request.method == "POST":
-		return create_namespace(namespace)
+    if request.method == "POST":
+        return create_namespace(namespace)
 
-	elif request.method == "GET":
-		return get_namespace(namespace)
+    elif request.method == "GET":
+        return get_namespace(namespace)
 
-	elif request.method == "DELETE":
-		return delete_namespace(namespace)
+    elif request.method == "DELETE":
+        return delete_namespace(namespace)
 
-#네임스페이스 생성
+# 네임스페이스 생성
+
+
 def create_namespace(namespace):
-	body = {
-		"metadata" : {
-			"name" : namespace
-		}
-	}
-	try:
-		core_v1.create_namespace(body=body)
-	except ApiException as e:
-		return message_handler(message="fail to CREATE namespace", exception=e)
-	return message_handler(message="namespace created")
+    body = {
+        "metadata": {
+            "name": namespace
+        }
+    }
+    try:
+        core_v1.create_namespace(body=body)
+    except ApiException as e:
+        return message_handler(message="fail to CREATE namespace", exception=e)
+    return message_handler(message="namespace created")
 
-#네임스페이스 조회
+# 네임스페이스 조회
+
+
 def get_namespace(namespace):
-    ret=core_v1.read_namespace(namespace)
-    list_namespace=[]
+    ret = core_v1.read_namespace(namespace)
+    list_namespace = []
     for i in ret.items:
         list_namespace.append(i.metadata.name)
-	example = {'list_namespace':list_namespace}
-	return example
+        example = {'list_namespace': list_namespace}
+        return example
 
-#네임스페이스 삭제
+# 네임스페이스 삭제
+
+
 def delete_namespace(namespace):
-	try:
-		core_v1.delete_namespace(namespace)
-	except ApiException as e:
-		return message_handler(message="fail to DELETE namespace", exception=e)
+    try:
+        core_v1.delete_namespace(namespace)
+    except ApiException as e:
+        return message_handler(message="fail to DELETE namespace", exception=e)
 
-#return할 메시지를 딕셔너리 형태로 처리
+# return할 메시지를 딕셔너리 형태로 처리
+
+
 def message_handler(**kwargs):
-	if kwargs['exception'] not in kwargs.keys():
-		return {'message' : kwargs['message']}
-	else:
-		return {'message' : kwargs['message'], 'error' : kwargs['exception']}
+    if kwargs['exception'] not in kwargs.keys():
+        return {'message': kwargs['message']}
+    else:
+        return {'message': kwargs['message'], 'error': kwargs['exception']}
+
 
 '''
 기존 코드 임시 주석처리 추후 삭제예정
--- API/app.py
+++ API/app.py
@@ -4,11 +4,11 @@ from api import core_v1
 
 app = Flask(__name__)
 
-#namespace 관련
+# namespace 관련
 app.register_blueprint(namespace.namespace_api, url_prefix='/namespace')
 app.register_blueprint(deployment.deployment_api, url_prefix='/deployment')
 
-#기본 페이지
+# 기본 페이지
 @app.route("/")
 def welcome():
     return "welcome to service"
@@ -16,14 +16,16 @@ def welcome():
 # pod 조회 예시
 @app.route("/pods")
 def get_deploy():
-    podlist=[]
+    podlist = []
     print("Listing pods and IPs")
     ret = core_v1.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
-        podlist.append([i.status.pod_ip, i.metadata.namespace, i.metadata.name])
-    example={'podlist':podlist}
+        podlist.append(
+            [i.status.pod_ip, i.metadata.namespace, i.metadata.name])
+    example = {'podlist': podlist}
     return example
 
-#host='0.0.0.0',debug=True, port=80
+
+# host='0.0.0.0',debug=True, port=80
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=80)
