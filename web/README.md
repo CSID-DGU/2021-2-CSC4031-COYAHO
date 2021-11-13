1113c23a00410c692a8cd241cd98aa8b63c00b67
Yujin Bae <ebeuy8636@gmail.com>
2021-11-13 22:23:09 +0900

    login & logout

---
+++ web/README.md
@@ -0,0 +1,2 @@
+### 2021.11.13 Demo
+![demo1113](https://user-images.githubusercontent.com/65498159/141645339-796b82d5-bc2e-4d7a-b984-8c3eb169b602.gif)
---
+++ web/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from models import Fcuser
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
+
+class RegisterForm(FlaskForm):
+    userid = StringField('userid', validators=[DataRequired()])
+    # username = StringField('username', validators=[DataRequired()])
+    grafana_ip = StringField('grafana_ip', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(), EqualTo('re_password')]) #equalTo("필드네임")
+    re_password = PasswordField('re_password', validators=[DataRequired()])
+
+
+class LoginForm(FlaskForm):
+    class UserPassword(object):
+        def __init__(self, message=None):
+            self.message = message
+        def __call__(self,form,field):
+            userid = form['userid'].data
+            password = field.data
+            fcuser = Fcuser.query.filter_by(userid=userid).first()
+            if fcuser.password != password:
+                # raise ValidationError(message % d)
+                raise ValueError('Wrong password')
+    userid = StringField('userid', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(), UserPassword()]) 
