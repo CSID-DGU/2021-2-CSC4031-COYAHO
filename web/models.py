3e6fe0620a29cff0dd41451b20ee02a93e14b0e4
SEOK IN SHIM <70099284+shimdol22@users.noreply.github.com>
2021-11-15 20:47:34 +0900

    Add files via upload

--- web/models.py
+++ web/models.py
@@ -1,10 +1,20 @@
-from flask_sqlalchemy import SQLAlchemy
+##데이터베이스와 관련된 코드
 
-db = SQLAlchemy()          
+from operator import index
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()           #SQLAlchemy를 사용해 데이터베이스 저장
 
 class Fcuser(db.Model): 
-    __tablename__ = 'fcuser'   
-    id = db.Column(db.Integer, primary_key = True) 
-    userid = db.Column(db.String(32))
-    password = db.Column(db.String(64)) 
-    grafana_ip = db.Column(db.String(8))
+    __tablename__ = 'fcuser'   #테이블 이름 : fcuser
+    #id = db.Column(db.Integer, primary_key = True, )   #id를 프라이머리키로 설정
+    userid = db.Column(db.String(32), primary_key = True, index = True)       #이하 위와 동일
+    password = db.Column(db.String(32), index = True)     #패스워드를 받아올 문자열길이 
+    username = db.Column(db.String(8), index = True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
