8f06a16b0f0132e277cab208fc25c2fc5419228e
kimbk209 <64012235+kimbk209@users.noreply.github.com>
2021-11-17 00:59:33 +0900

    복구 기능 수정

--- API/api/__init__.py
+++ API/api/__init__.py
@@ -3,5 +3,6 @@ from kubernetes import client, config
 
 # 쿠버네티스 incluster 인증
 config.load_incluster_config()
+kube_config = client
 core_v1 = client.CoreV1Api()
 apps_v1 = client.AppsV1Api()
--- API/api/restore.py
+++ API/api/restore.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, request
 from kubernetes import utils
+from api import kube_config
 
 restore_api = Blueprint('restore', __name__)
 
 
-@restore_api.route('/do_restore', methods=['GET'])
-def restore():
-    utils.create_from_yaml.create_from_yaml()
+@restore_api.route('/post', methods=['POST'])
+def post():
+    yaml_data = request.get_json()
+    utils.create_from_yaml(k8s_client=kube_config, yaml_objects=[yaml_data])
+    return {'yaml': yaml_data}
