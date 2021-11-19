48637c0f414dd4d8df612218a56e64920e635602
kimbk209 <64012235+kimbk209@users.noreply.github.com>
2021-11-19 19:10:51 +0900

    send_request 수정

--- web/app.py
+++ web/app.py
@@ -3,6 +3,7 @@ from models import db
 import os
 import requests
 import yaml
+import time
 from models import Fcuser
 from flask import session
 from flask_wtf.csrf import CSRFProtect
@@ -89,16 +90,25 @@ def uploader_file():
 @app.route('/recovery')
 def recovery():
     # 쿼리스트링으로 ip주소 받음
+    target_namespace = request.args.get('namespace')  # 이 부분 추가작성 필요
     ip_address = request.args.get('ip_address')
     # 해당 아이피로 전송
     yaml_file_dir = './yaml/recovery.yaml'
 
+    sample_headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.1.2222.33 Safari/537.36",
+        "Accept-Encoding": "*",
+        "Connection": "keep-alive"
+    }
+
     def send_request(target_URL=None, kind=None, yaml_data=None):
-        requests.post(target_URL+'/'+kind+'/post', json=yaml_data)
+        requests.post(target_URL+'/'+kind +
+                      f'/post?namespace={target_namespace}', json=yaml_data, headers=sample_headers)
 
     with open(os.path.join(os.path.dirname(__file__), yaml_file_dir)) as f:
         dep = list(yaml.safe_load_all(f))
         for i in range(len(dep)):
+            time.sleep(1)
             kind = dep[i]['kind'].lower()
             send_request(target_URL=ip_address,
                          kind=kind, yaml_data=(dep[i]))
